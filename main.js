bd322d95b3f08b3ab0463310a96af694fca1e14a
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2022-12-03 20:58:33 +0200

    start hw

---
+++ main.js
@@ -0,0 +1,27 @@
+let productCounterEl = document.getElementById("product-counter");
+let btnAddToCard = document.querySelectorAll(".btn-add-to-card");
+
+btnAddToCard.forEach((item) => {
+  item.addEventListener("click", function () {
+    productCounterEl.textContent = +productCounterEl.textContent + 1;
+  });
+});
+
+let btnMoreDetails = document.querySelectorAll(".btn-more-details");
+let modal = document.querySelector(".modal-box");
+let modalClose = document.getElementById("close");
+
+btnMoreDetails.forEach((item) => {
+  item.addEventListener("click", openModal);
+});
+
+modalClose.addEventListener("click", closeModal);
+
+function openModal() {
+  modal.classList.remove("hide");
+  modal.classList.add("show");
+}
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+}
